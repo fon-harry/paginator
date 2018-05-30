371ca0ed9f78e62141021dc7833cd4bb5b47e9f9
fon_harry <fon.harry@gmail.com>
2018-05-30 14:00:06 +0300

    added getAllData()

--- index.js
+++ index.js
@@ -5,14 +5,7 @@ let mysql = require('mysql')
 app.set('views', './views')
 app.set('view engine', 'pug')
 
-app.get('/index', function (req, res) {
-  res.render('index', {
-    title: 'Hey',
-    message: 'Hello there!'
-  })
-})
-
-app.get('/', function (req, res) {
+function getAllData (callback) {
   let connection = mysql.createConnection({
     host: 'localhost',
     user: 'nodejs',
@@ -26,10 +19,23 @@ app.get('/', function (req, res) {
 
   connection.query('SELECT * from world.city', (error, results, fields) => {
     if (error) throw error
-    res.send(JSON.stringify(results))
+    callback(JSON.stringify(results))
   })
 
   connection.end()
+}
+
+app.get('/', function (req, res) {
+  getAllData((data) => {
+    res.send(data)
+  })
+})
+
+app.get('/index', function (req, res) {
+  res.render('index', {
+    title: 'Hey',
+    message: 'Hello there!'
+  })
 })
 
 app.listen(3000, function () {
